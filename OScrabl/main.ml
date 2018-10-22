76c4ae904be9da5d470b5cabf06f6f2bc7af96bc
Richard Yu <ry275@cornell.edu>
2018-10-22 19:17:45 -0400

    minor main fixes

--- OScrabl/main.ml
+++ OScrabl/main.ml
@@ -11,20 +11,22 @@ let rec gameplay board iteration =
     print_newline();
     print_string "place, quit";
     print_newline(); *)
-  if iteration = 0 then 
-    (ANSITerminal.(print_string [red] 
-                     "\n\nWelcome to OScrabl!\n");
-     gameplay board (iteration + 1))
+  if iteration = 0 then (
+    (ANSITerminal.resize 110 39);
+    gameplay board (iteration + 1))
   else
+    let not_used = Sys.command"clear" in
+    (ANSITerminal.(print_string [red] 
+                     "\n\n             OScrable by Richard Yu, Samuel Levenson, and Max Chen \n"));
     print_board board;
-  (match (parse_cmd (read_line ())) with
-   | Quit -> 
-     (*print_string "Thanks for playing OScrabl!";*)
-     print_newline(); 
-     exit 0
-   | Place (tile,(row,col)) -> gameplay (insertTile board (Some {letter=tile; value= 5}) (row,col)) (iteration + 1)
-   | _ -> exit 0
-  )
+    (match (parse_cmd (read_line ())) with
+     | Quit -> 
+       (*print_string "Thanks for playing OScrabl!";*)
+       print_newline(); 
+       exit 0
+     | Place (tile,(row,col)) -> gameplay (insertTile board (Some {letter=tile; value= 5}) (row,col)) (iteration + 1)
+     | _ -> exit 0
+    )
 
 
 
