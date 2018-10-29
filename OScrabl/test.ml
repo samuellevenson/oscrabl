9ff571d6c0d17355d7e5c3003062f273db5c5e2f
Max <mlc294@cornell.edu>
2018-10-29 15:14:54 -0400

    OUnit testing hash tables

--- OScrabl/test.ml
+++ OScrabl/test.ml
@@ -29,9 +29,48 @@ let action_tests =
   ]
 
 
+let test_empty_set_1 : (int Words.StringHashTbl.t) = 
+  Words.StringHashTbl.create (3)
+let test_empty_set_2 : (int Words.StringHashTbl.t) = 
+  Words.StringHashTbl.create (3)
+let test_empty_set_3 : (int Words.StringHashTbl.t) = 
+  Words.StringHashTbl.create (3)
+
+let make_add_set_tests
+    (name: string)
+    (arr: string array)
+    (set: int Words.StringHashTbl.t)
+    (exp: int Words.StringHashTbl.t) =
+  name >:: (fun _ -> 
+      (Words.add_hash_set set arr Hashtbl.hash); 
+      Words.StringHashTbl.add test_empty_set_2 "1" (Hashtbl.hash "1");
+      Words.StringHashTbl.add test_empty_set_2 "2" (Hashtbl.hash "2");
+      Words.StringHashTbl.add test_empty_set_2 "3" (Hashtbl.hash "3");
+      assert_equal exp set;
+    )
+
+let make_validity_tests
+    (name: string)
+    (str: string)
+    (set: int Words.StringHashTbl.t)
+    (exp: bool) =
+  name >:: (fun _ -> 
+      Words.add_hash_set test_empty_set_3 [|"4"; "5"; "6"|] Hashtbl.hash;
+      assert_equal exp (Words.validity str set); 
+    )
+
+let word_tests =
+  [
+    make_add_set_tests "1" [|"1"; "2"; "3"|] test_empty_set_1 test_empty_set_2;
+    make_validity_tests "valid 1" "4" test_empty_set_3 true;
+    make_validity_tests "valid 2" "5" test_empty_set_3 true;
+    make_validity_tests "valid 3" "1" test_empty_set_3 false;
+    make_validity_tests "valid 4" "99" test_empty_set_3 false;
+  ]
 let suite =
   "OScrabl test suite"  >::: List.flatten [
     action_tests;
+    word_tests;
   ]
 
 let _ = run_test_tt_main suite
