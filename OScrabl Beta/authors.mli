96e7d64ff9a68298dfc4da13c4e9e53d2f61e728
Samuel Levenson <shl96@cornell.edu>
2018-11-02 01:32:36 -0400

    removed oscrabl beta folder–now its just a branch

--- OScrabl Beta/authors.mli
+++
@@ -1,7 +0,0 @@
-(**
-   @author Max Chen (mlc294), Samuel Levenson (shl96), Richard Yu (ry275)
-*)
-
-(** [hours_worked] is a list of the number of hours each team member
-    worked on this assignment. *)
-val hours_worked : int list
