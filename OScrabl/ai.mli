b189a9688e407c9f2a4708648e419fb7f030e2bb
Max <mlc294@cornell.edu>
2018-11-06 16:37:22 -0500

    AI and Main mli files

---
+++ OScrabl/ai.mli
@@ -0,0 +1,13 @@
+open Actions
+open Moment
+open Words
+open Board
+
+exception InvalidSize
+exception Test
+
+(** the type for ai a*)
+type brain 
+
+(** [ai_actions] is the actions that the AI will perform. *)
+val ai_actions : Moment.t -> Actions.action list
---
+++ OScrabl/main.mli
@@ -0,0 +1,2 @@
+(** [initiate_game] starts the game and allows the user to begin input. *)
+val initiate_game : unit -> unit
