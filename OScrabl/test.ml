25c79f67d492d3b6065329ba781af6d5a995f6ad
Max <mlc294@cornell.edu>
2018-11-07 00:07:00 -0500

    Adjust test cases

--- OScrabl/test.ml
+++ OScrabl/test.ml
@@ -48,7 +48,14 @@ let exchange_state_2 =
                                            {letter = "F"; value = 1};
                                            {letter = "G"; value = 1}]) 0
   in
-  create_moment emptyBoard [] [new_player] new_player []
+  create_moment emptyBoard ([{letter = "H"; value = 1};
+                             {letter = "H"; value = 1};
+                             {letter = "H"; value = 1};
+                             {letter = "H"; value = 1};
+                             {letter = "H"; value = 1};
+                             {letter = "H"; value = 1};
+                             {letter = "W"; value = 1}])
+    [new_player] new_player []
 
 let placement_state_1 =
   let new_player = create_player "Test1" ([{letter = "B"; value = 1};
@@ -75,7 +82,8 @@ let placement_state_2 =
 
 let pipeline_place tuple state = place_tile state tuple
 
-let place_tuple letter row col = (letter, (single_to_int row, int_of_string col)) 
+let place_tuple letter row col = 
+  (letter, (single_to_int row, int_of_string col)) 
 
 
 let end_turn_1 = 
@@ -119,6 +127,11 @@ let test_exchange_1 =
                                            {letter = "G"; value = 1};]) 0 
   in
   create_moment emptyBoard ([{letter = "H"; value = 1};
+                             {letter = "H"; value = 1};
+                             {letter = "H"; value = 1};
+                             {letter = "H"; value = 1};
+                             {letter = "H"; value = 1};
+                             {letter = "H"; value = 1};
                              {letter = "W"; value = 1}]) 
     [curr_player; dummy_player] curr_player ["Started Game"]
 
@@ -146,14 +159,14 @@ let rule_tests =
   [ 
     make_exchange_tests "Swapping letters" 
       test_exchange_1
-      ["A";"B";] 
+      ["A";"B";"C";"D";"E";"F";"G"] 
       [ 
-        {letter = "C"; value = 1};
-        {letter = "D"; value = 1};
-        {letter = "E"; value = 1};
-        {letter = "F"; value = 1};
-        {letter = "G"; value = 1};
         {letter = "W"; value = 1};
+        {letter = "H"; value = 1};
+        {letter = "H"; value = 1};
+        {letter = "H"; value = 1};
+        {letter = "H"; value = 1};
+        {letter = "H"; value = 1};
         {letter = "H"; value = 1};
       ];
     make_exchange_failures "Invalid Exchange Selection" 
@@ -163,14 +176,19 @@ let rule_tests =
     make_exchange_failures "Invalid Exchange Selection, but don't have 7 tiles." 
       test_exchange_2 ["H";"W"] InvalidExchange;
     make_placement_tests "First tile is not h7" 
-      (place_tile placement_state_1 ("A",(single_to_int "H", int_of_string "8")))
+      (place_tile placement_state_1 
+         ("A",(single_to_int "H", int_of_string "8")))
       InvalidTilePlacement; 
     make_placement_tests "Invalid Word" 
-      (place_tile (place_tile placement_state_1 ("A",(single_to_int "H", int_of_string "7")))
+      (place_tile 
+         (place_tile placement_state_1 
+            ("A",(single_to_int "H", int_of_string "7")))
          ("Z",(single_to_int "H", int_of_string "8")))
       (InvalidWord "AZ"); 
     make_placement_tests "Non-connected placement" 
-      (place_tile (place_tile placement_state_1 ("A",(single_to_int "H", int_of_string "7")))
+      (place_tile 
+         (place_tile placement_state_1 
+            ("A",(single_to_int "H", int_of_string "7")))
          ("T",(single_to_int "M", int_of_string "0")))
       (InvalidTilePlacement); 
     make_placement_tests "Triangular Placement" 
@@ -210,7 +228,8 @@ let action_tests =
   [
     make_parse_failures "1" "aasd asdsa" Broken;
     make_parse_failures "2" "" Blank;
-    make_parse_tests "3" "place a o 10" (Actions.Place ("A",(single_to_int "O",10)));
+    make_parse_tests "3" "place a o 10" 
+      (Actions.Place ("A",(single_to_int "O",10)));
     make_parse_tests "4" "quit" (Actions.Quit);
     make_parse_failures "5" "place duf" Broken;
     make_parse_failures "6" "placeasd" Broken;
@@ -271,9 +290,10 @@ let make_refill_tests
     )
 
 let state_3_elements : Moment.t = 
-  let curr_player = create_player "OScrabl Player" ([{letter = "A"; value = 1};
-                                                     {letter = "A"; value = 1};
-                                                     {letter = "A"; value = 1};]) 0 in
+  let curr_player = create_player "OScrabl Player" 
+      ([{letter = "A"; value = 1};
+        {letter = "A"; value = 1};
+        {letter = "A"; value = 1};]) 0 in
   create_moment emptyBoard init_bag [curr_player] curr_player []
 
 let state_0_elements : Moment.t = 
@@ -281,13 +301,14 @@ let state_0_elements : Moment.t =
   create_moment emptyBoard init_bag [curr_player] curr_player []
 
 let state_7_elements : Moment.t = 
-  let curr_player = create_player "OScrabl Player" ([{letter = "A"; value = 1};
-                                                     {letter = "A"; value = 1};
-                                                     {letter = "A"; value = 1};
-                                                     {letter = "A"; value = 1};
-                                                     {letter = "A"; value = 1};
-                                                     {letter = "A"; value = 1};
-                                                     {letter = "A"; value = 1};]) 0 in
+  let curr_player = create_player "OScrabl Player" 
+      ([{letter = "A"; value = 1};
+        {letter = "A"; value = 1};
+        {letter = "A"; value = 1};
+        {letter = "A"; value = 1};
+        {letter = "A"; value = 1};
+        {letter = "A"; value = 1};
+        {letter = "A"; value = 1};]) 0 in
   create_moment emptyBoard init_bag [curr_player] curr_player []
 
 
